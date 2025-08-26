0133e3f89f32bdd896248e1d3cfa95e05fe28ecc
max <max@bvm.network>
2025-08-26 15:09:21 +0700

    Add ScheduledTransfersCard component to HomePage; enhance layout with improved spacing and dynamic rendering of scheduled transfers for better user experience.

---
+++ src/modules/HomePage/ScheduledTransfers/Card/index.tsx
@@ -0,0 +1,56 @@
+import Label from "@/components/Typo/Label";
+import { TypoColor } from "@/enums/typo";
+import { Box, Flex } from "@chakra-ui/react";
+import Image from "next/image";
+
+type Props = {
+  isLast?: boolean;
+};
+
+function ScheduledTransfersCard({ isLast }: Props): React.ReactElement {
+  return (
+    <Box
+      py={"1.2rem"}
+      borderBottom={isLast ? "none" : "1px solid var(--border-color)"}
+    >
+      <Flex alignItems={"center"} justifyContent={"space-between"}>
+        <Flex alignItems={"center"} gap={"1.5rem"}>
+          <Box
+            width={"3.3rem"}
+            height={"3.3rem"}
+            css={{
+              "& img": {
+                width: "100%",
+                height: "100%",
+              },
+            }}
+          >
+            <Image src={"/imgs/avt.png"} alt="user" width={33} height={33} />
+          </Box>
+          <Flex flexDirection={"column"} gap={"0.7rem"}>
+            <Label
+              color={TypoColor.primaryTextColor}
+              size={14}
+              fontWeight={600}
+            >
+              Saleh Ahmed
+            </Label>
+            <Label
+              color={TypoColor.secondaryTextColor}
+              size={12}
+              fontWeight={500}
+            >
+              saleh@gmail.com
+            </Label>
+          </Flex>
+        </Flex>
+
+        <Label color={TypoColor.primaryTextColor} size={16} fontWeight={600}>
+          - $435,00
+        </Label>
+      </Flex>
+    </Box>
+  );
+}
+
+export default ScheduledTransfersCard;
--- src/modules/HomePage/ScheduledTransfers/index.tsx
+++ src/modules/HomePage/ScheduledTransfers/index.tsx
@@ -1,14 +1,22 @@
 import ViewAllButton from "@/components/Buttons/ViewAllButton";
 import HeadingSection from "@/components/HeadingSection";
+import ScheduledTransfersCard from "@/modules/HomePage/ScheduledTransfers/Card";
 import { Box, Flex } from "@chakra-ui/react";
 
 function ScheduledTransfers(): React.ReactElement {
   return (
     <Box>
-      <Flex alignItems={"center"} justifyContent={"space-between"}>
+      <Flex
+        alignItems={"center"}
+        justifyContent={"space-between"}
+        mb={"2.5rem"}
+      >
         <HeadingSection title="Scheduled Transfers" />
         <ViewAllButton href={"/transaction"} />
       </Flex>
+      {Array.from({ length: 5 }).map((_, index) => (
+        <ScheduledTransfersCard key={index} isLast={index === 4} />
+      ))}
     </Box>
   );
 }
